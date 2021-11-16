72b372c21e63efb197c3557aa0cae0c263c696f0
Mateusz <mateusz.mazewski.stud@pw.edu.pl>
2021-11-16 16:55:34 +0100

    Added generating example publishers data

--- src/main/java/com/github/mateuszmazewski/library/data/generator/DataGenerator.java
+++ src/main/java/com/github/mateuszmazewski/library/data/generator/DataGenerator.java
@@ -3,9 +3,11 @@ package com.github.mateuszmazewski.library.data.generator;
 import com.github.mateuszmazewski.library.data.entity.Author;
 import com.github.mateuszmazewski.library.data.entity.Category;
 import com.github.mateuszmazewski.library.data.entity.Genre;
+import com.github.mateuszmazewski.library.data.entity.Publisher;
 import com.github.mateuszmazewski.library.data.repository.AuthorRepository;
 import com.github.mateuszmazewski.library.data.repository.CategoryRepository;
 import com.github.mateuszmazewski.library.data.repository.GenreRepository;
+import com.github.mateuszmazewski.library.data.repository.PublisherRepository;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,10 +20,13 @@ public class DataGenerator {
     @Bean
     public CommandLineRunner loadData(AuthorRepository authorRepository,
                                       CategoryRepository categoryRepository,
-                                      GenreRepository genreRepository) {
+                                      GenreRepository genreRepository,
+                                      PublisherRepository publisherRepository) {
 
         return args -> {
             Logger logger = LoggerFactory.getLogger(getClass());
+
+            // ----- GENRES -----
 
             Genre epic = new Genre("Epika");
             Genre poetry = new Genre("Liryka");
@@ -37,6 +42,8 @@ public class DataGenerator {
             }
 
             logger.info("Generating demo data");
+
+            // ----- AUTHORS -----
 
             Author a1 = new Author();
             a1.setName("Adam");
@@ -54,6 +61,8 @@ public class DataGenerator {
             authorRepository.save(a2);
             authorRepository.save(a3);
 
+            // ----- CATEGORIES -----
+
             Category c1 = new Category();
             c1.setGenre(epic);
             c1.setName("Powieść");
@@ -69,6 +78,27 @@ public class DataGenerator {
             categoryRepository.save(c1);
             categoryRepository.save(c2);
             categoryRepository.save(c3);
+
+            // ----- PUBLISHERS -----
+
+            Publisher p1 = new Publisher();
+            p1.setName("Wydawnictwa Szkolne i Pedagogiczne");
+            p1.setEmail("wsip@wsip.com.pl");
+            p1.setPhoneNumber("801 220 555");
+
+            Publisher p2 = new Publisher();
+            p2.setName("Nowa Era");
+            p2.setEmail("nowaera@nowaera.pl");
+            p2.setPhoneNumber("801 88 10 10");
+
+            Publisher p3 = new Publisher();
+            p3.setName("Wydawnictwo Czarne");
+            p3.setEmail("redakcja@czarne.com.pl");
+            p3.setPhoneNumber("18 353 58 93");
+
+            publisherRepository.save(p1);
+            publisherRepository.save(p2);
+            publisherRepository.save(p3);
         };
     }
 
