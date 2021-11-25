6d2a1b542a863628283ae8e16b1e2b3e50195cc9
Mateusz <mateusz.mazewski.stud@pw.edu.pl>
2021-11-25 21:22:39 +0100

    Added getAuthenticatedUser() method

--- src/main/java/com/github/mateuszmazewski/library/security/SecurityService.java
+++ src/main/java/com/github/mateuszmazewski/library/security/SecurityService.java
@@ -2,11 +2,24 @@ package com.github.mateuszmazewski.library.security;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.server.VaadinServletRequest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityService {
+
+    public static UserDetails getAuthenticatedUser() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Object principal = context.getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return (UserDetails) principal;
+        }
+        // Anonymous or no authentication.
+        return null;
+    }
 
     public void logout() {
         UI.getCurrent().getPage().setLocation("/");
